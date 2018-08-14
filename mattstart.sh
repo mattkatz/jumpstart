ff4c01ca967da530f9ce8a67cc93ca9f8f2da3d2
Matt Katz <Matthew.Katz@arcesium.com>
2018-08-14 18:51:57 -0400

    adding colors and tmux

--- mattstart.sh
+++ mattstart.sh
@@ -1,26 +1,32 @@
 #!/usr/bin/sh
+echo "lets use some colors"
+alias echo='echo -e'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
 BASEDIR=$(dirname "$0")
-echo "Running from $BASEDIR"
+echo "${GREEN}Running from $BASEDIR${NC}"
+
 # make sure to install python2, python3, vim, git, tmux, pipenv, wget, curl
 if command -v curl >/dev/null 2>&1
 then
-	echo "curl is installed, tight."
+	echo "${GREEN}curl is installed, tight.${NC}"
 else
-	echo "curl isn't installed, get that done!"
+	echo "${RED}curl isn't installed, get that done!${NC}"
   exit 1
 fi
 if command -v wget >/dev/null 2>&1
 then
-	echo "wget is installed, sweet."
+	echo "${GREEN}wget is installed, sweet.${NC}"
 else
-	echo "whoa, wget isn't installed, get that done!"
+	echo "${RED}whoa, wget isn't installed, get that done!${NC}"
   exit 1
 fi
 # Set up zsh by cloning oh-my-zsh
 if [ -d "$HOME/.oh-my-zsh" ]
 then
-	echo "oh-my-zsh already exists!"
+	echo "${GREEN}oh-my-zsh already exists!${NC}"
 else
 	echo "installing oh-my-zsh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -29,7 +35,7 @@ fi
 # clone my vim config
 if [ -d "$HOME/.vim" ]
 then
-	echo "vim config directory already exists"
+	echo "${GREEN}vim config directory already exists${NC}"
 else
 	echo "Cloning my vim config"
 	git clone https://github.com/mattkatz/.vim
@@ -40,29 +46,40 @@ fi
 # add our gitignore
 if [ -e "$HOME/.gitignore" ]
 then
-  echo "Good, we have a user .gitgnore file"
+  echo "${GREEN}Good, we have a user .gitgnore file${NC}"
 else
   echo "link our gitignore file in"
   ln -s $BASEDIR/.gitignore ~/.gitignore
 fi
 
-echo "make sure our gitignore is used by git!"
+echo "making sure our gitignore is used by git!"
 git config --global core.excludesfile $HOME/.gitignore
 
 # set up tmux by cloning oh-my-tmux
 if command -v tmux >/dev/null 2>&1
 then
-  echo "Whoot, tmux is installed"
+  echo "${GREEN}Whoot, tmux is installed${NC}"
 else
-  echo "Oh NO, please install tmux. Try:"
-  echo "sudo apt-get install tmux"
+  echo "${RED}Oh NO, please install tmux. Try:${NC}"
+  echo "${RED}sudo apt-get install tmux${NC}"
+fi
+if [ -d "$HOME/.tmux" ]
+then
+  echo "${GREEN}Excellent, oh-my-tmux is already setup${NC}"
+else
+  echo "Installing oh-my-tmux"
+  pushd $HOME
+  git clone https://github.com/gpakosz/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  cp .tmux/.tmux.conf.local .
+  echo "${GREEN}Installed oh-my-tmux!${NC}"
 fi
 
 
 # gotta have some local user binaries
 if [ -d "$HOME/.local" ]
 then 
-  echo ".local exists" 
+  echo "${GREEN}.local exists"${NC} 
 else
   echo "making .local"
   mkdir ~/.local
@@ -70,7 +87,7 @@ fi
 
 if [ -d "$HOME/.local/bin" ]
 then 
-  echo ".local/bin exists" 
+  echo "${GREEN}.local/bin exists"${NC} 
 else
   echo "making .local/bin"
   mkdir ~/.local/bin
@@ -79,7 +96,7 @@ fi
 # add the .local/bin to the path
 if echo $PATH | grep -q .local/bin
 then
-  echo ".local/bin is on the path, whew!"
+  echo "${GREEN}.local/bin is on the path, whew!${NC}"
 else
   echo "let's add .local/bin to the path"
   ln -s $BASEDIR/.pathrc $HOME/.oh-my-zsh/custom/.pathrc
@@ -90,7 +107,7 @@ BINDIR=$HOME/.local/bin
 # install entr to the .local/bin
 if command -v entr >/dev/null 2>&1
 then
-  echo "huzzah, entr is installed"
+  echo "${GREEN}huzzah, entr is installed${NC}"
 else
   echo "installing entr locally"
   mkdir  $BASEDIR/entr-source
