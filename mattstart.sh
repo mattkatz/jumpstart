f7c111d42029d01fa958f414bfb9e083d6dec507
Matt Katz <Matthew.Katz@arcesium.com>
2018-08-14 16:06:15 -0400

    vim config cloning

--- mattstart.sh
+++ mattstart.sh
@@ -3,10 +3,13 @@
 # Set up zsh by cloning oh-my-zsh
 # set up tmux by cloning oh-my-tmux
 # clone my vim config
-if [ -d "~/.vim" ]
+if [ -d ".vim" ]
 then
-	echo "Cloning my vim config"
-else
 	echo "vim config directory already exists"
+else
+	echo "Cloning my vim config"
+	git clone https://github.com/mattkatz/.vim
+	ln -s ~/.vim/.vimrc ~/.vimrc
+	echo "plugins should install on next launch of vim"
 fi
 # install my vim plugins
