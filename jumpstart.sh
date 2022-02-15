66eaa8028f93acf495c480b5693e805055cf77ff
mattkatz <mattkatz@morelightmorelight.com>
2022-02-15 15:46:02 -0500

    adding a git gone alias

--- jumpstart.sh
+++ jumpstart.sh
@@ -492,3 +492,6 @@ else
   yellow "installing gping for ping graphing"
   brew install gping
 fi
+
+# add a git gone alias
+git config --global alias.gone "! git fetch -p && git for-each-ref --format '%(refname:short) %(upstream:track)' | awk '\$2 == \"[gone]\" {print \$1}' | xargs -r git branch -D"
